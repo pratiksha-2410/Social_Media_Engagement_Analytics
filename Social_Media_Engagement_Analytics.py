f5c13dfbfdbe8b59ed3a93e2783e974ce4eed0ce
pratiksha zagade <pratikshazagadee@gmail.com>
2026-02-23 22:54:16 +0530

    Load Dataset

--- Social_Media_Engagement_Analytics.py
+++ Social_Media_Engagement_Analytics.py
@@ -4,3 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Load Dataset
+
+df = pd.read_csv("social_media_engagement_advanced.csv")
+print(df.head())
