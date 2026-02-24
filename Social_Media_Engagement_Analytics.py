78427cd8981ac2b47f9b1d56ea6fdf7f4e5c496e
pratiksha zagade <pratikshazagadee@gmail.com>
2026-02-24 15:14:39 +0530

    Major Changes & Max Engagement Post ID

--- Social_Media_Engagement_Analytics.py
+++ Social_Media_Engagement_Analytics.py
@@ -4,10 +4,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sqlite3
+
+conn = sqlite3.connect("social_media.db")
+cursor = conn.cursor()
+
+
 
 # Load Dataset
 
 df = pd.read_csv("social_media_engagement_advanced.csv")
+df.to_sql("orders",conn,if_exists="replace",index=False)
 print(df.head())
 
 # Data Featuring
@@ -16,12 +23,43 @@ print("\n Data Shape:",df.shape)
 print("\n Data Size:",df.size)
 print("\n Data info:",df.info())
 print("\n Data Describtion:",df.describe())
+print(df.columns)
 
 # Data Cleaning
 
 print("\n Null values:",df.isna().sum())
 
-# Find Duplicate Values & Remove them
+# Finding Duplicate Values & Remove them
 
 print("\n Duplicate Values:",df.duplicated().sum())
 print("\n",df.drop_duplicates())
+
+# Calculate Engagement Rate 
+
+df["Total_Engagemnet"] = df["Likes"] + df["Comments"] + df["Shares"]
+df["Engagement_Rate"] = (df["Total_Engagement"] / df["Reach"]) * 100
+print(df[["Post_ID","Engagement_Rate"]])
+
+
+
+# Max Engagement Post_ID via Pandas
+max_index = df["Engagement_Rate"].idxmax()
+max_post_ID = df.loc[max_index,"Post_ID"]
+max_engagement_rate = df.loc[max_index,"Engagement_Rate"]
+
+print("Post with Highest Engagement")
+print("Post_ID",max_post_ID)
+print("Engagement_Rate",max_engagement_rate )
+
+# Max Engagement Post_ID via SQL
+
+df.rename(columns={"Engagement_Rate (%)":"Engagementt_Rate"},inplace=True)
+df.to_sql("orders",conn,if_exists="replace",index=False)
+
+query = """SELECT Post_ID,Engagementt_Rate 
+FROM orders
+WHERE Engagementt_Rate = (SELECT MAX(Engagementt_Rate)
+FROM orders)""" 
+
+Max_Post_ID = pd.read_sql(query,conn)
+print(Max_Post_ID)
